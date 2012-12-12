a569efd87d08ca0a067aa5aeecb1baaea54ecf4d
Daniel Bruder <daniel.bruder@gmail.com>
2012-12-13 00:09:44 +0100

    switching to new role ::merged

--- lib/Dist/Zilla/PluginBundle/Author/DBR.pm
+++ lib/Dist/Zilla/PluginBundle/Author/DBR.pm
@@ -3,16 +3,19 @@ package Dist::Zilla::PluginBundle::Author::DBR; # Make CPAN happy
 #  PODNAME: Dist::Zilla::PluginBundle::Author::DBR
 # ABSTRACT: DBRs Dist::Zilla PluginBundle
 
+# package Dist::Zilla::PluginBundle::Foobar;
+# Moose::with 'Dist::Zilla::Role::PluginBundle::Merged';
+# sub configure { shift->add_merged( qw[ Plugin1 Plugin2 Plugin3 Plugin4 ] ); }
+
+
 use MooseX::Declare;
+use true;
 
 class Dist::Zilla::PluginBundle::Author::DBR
- with Dist::Zilla::Role::PluginBundle::Easy
+ with Dist::Zilla::Role::PluginBundle::Merged
 {
 
-    use Dist::Zilla::PluginBundle::Filter;
-
     method configure {
-
         $self->add_bundle(
             Filter => {
                 -bundle => '@Classic',
@@ -21,36 +24,38 @@ class Dist::Zilla::PluginBundle::Author::DBR
             },
             # 'Apocalyptic',
         );
-
-        $self->add_plugins(
-            'ConfirmRelease',
-            'EOLTests',
-            'MetaJSON',
-            'ModuleBuild',
-            'NoTabsTests',
-            'ReadmeFromPod',
-            'TestRelease',
-            'AutoPrereqs',
-            'Test::ReportPrereqs',
-            'Test::CheckDeps',
-            'ReportPhase',
-            'ReportVersions',
-            'Test::MinimumVersion',
-            'CheckPrereqsIndexed',
-            'CheckVersionIncrement',
-            'SpellingCommonMistakesTests',
-            'Test::UseAllModules',
-            'MetaProvides::Class',
-            'SchwartzRatio',
-            'MetaTests',
-            'CheckExtraTests',
-            'RunExtraTests',
-            'InstallGuide',
-            'PodWeaver',
-            'MetaProvides::Package',
-            [ 'PruneCruft' => { 'except'    => '\.gitignore'  } ],
-            [ 'ExecDir'    => { 'dir'       => 'bin'          } ],
-            [ 'Authority'  => { 'authority' => 'cpan:DBR'     }],
+    
+        $self->add_merged(
+            qw[
+                =Dist::Zilla::Plugin::ConfirmRelease
+                =Dist::Zilla::Plugin::EOLTests
+                =Dist::Zilla::Plugin::MetaJSON
+                =Dist::Zilla::Plugin::ModuleBuild
+                =Dist::Zilla::Plugin::NoTabsTests
+                =Dist::Zilla::Plugin::ReadmeFromPod
+                =Dist::Zilla::Plugin::TestRelease
+                =Dist::Zilla::Plugin::AutoPrereqs
+                =Dist::Zilla::Plugin::Test::ReportPrereqs
+                =Dist::Zilla::Plugin::Test::CheckDeps
+                =Dist::Zilla::Plugin::ReportPhase
+                =Dist::Zilla::Plugin::ReportVersions
+                =Dist::Zilla::Plugin::Test::MinimumVersion
+                =Dist::Zilla::Plugin::CheckPrereqsIndexed
+                =Dist::Zilla::Plugin::CheckVersionIncrement
+                =Dist::Zilla::Plugin::SpellingCommonMistakesTests
+                =Dist::Zilla::Plugin::Test::UseAllModules
+                =Dist::Zilla::Plugin::MetaProvides::Class
+                =Dist::Zilla::Plugin::SchwartzRatio
+                =Dist::Zilla::Plugin::MetaTests
+                =Dist::Zilla::Plugin::CheckExtraTests
+                =Dist::Zilla::Plugin::RunExtraTests
+                =Dist::Zilla::Plugin::InstallGuide
+                =Dist::Zilla::Plugin::PodWeaver
+                =Dist::Zilla::Plugin::MetaProvides::Package
+            ],
+            PruneCruft => { 'except'    => '\.gitignore'  },
+            ExecDir    => { 'dir'       => 'bin'          },
+            Authority  => { 'authority' => 'cpan:DBR'     }
             # 'Test::Legal',
             # 'ExtraTests',
             # 'Test::Perl::Critic',
@@ -58,11 +63,10 @@ class Dist::Zilla::PluginBundle::Author::DBR
             # 'CheckChangesHasContent',
             # 'ChangeStats::Git',
             # 'Test::CheckManifest',
-            # [ 'Test::Portability' => { 'test_vms_length'  => 0,
-            #                            'test_ansi_chars'  => 0,
-            #                            'test_one_dot'     => 0                    } ],
-            # [ 'InstallRelease'    => { 'install_command'  => 'cpanm'              } ],
-            
+            # 'Test::Portability' => {  'test_vms_length'  => 0,
+            #                           'test_ansi_chars'  => 0,
+            #                           'test_one_dot'     => 0                   },
+            # 'InstallRelease'    => { 'install_command'  => 'cpanm'              },
         );
     }
 }
